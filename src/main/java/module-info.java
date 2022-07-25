858291693ec9096e301187ab084d9330625ed963
Nicolas Fassl <nicolas3750.fassl@gmail.com>
2022-07-25 09:05:09 +0200

    json improvements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires org.json;
 
 
     opens pokemon.userinterface to javafx.fxml;
--- src/main/java/pokemon/model/JsonReader.java
+++ src/main/java/pokemon/model/JsonReader.java
@@ -1,8 +1,13 @@
 package pokemon.model;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.json.*;
 
 public class JsonReader {
 
@@ -10,11 +15,39 @@ public class JsonReader {
 
     }
 
+    private String[] listFilesForFolder(File folder) {
+        ArrayList files = new ArrayList();
+        for (final File fileEntry : folder.listFiles()) {
+            files.add(fileEntry.getName());
+        }
+
+        return (String[]) files.toArray();
+    }
+
+    private static String readFileAsString(String file)
+    {
+        return new String(Files.readAllBytes(Paths.get(file)));
+    }
 
     private List<Pokemon> readAllPokemon(){
 
-        List<Pokemon> pokemonList = new ArrayList<>();
 
+        List<Pokemon> pokemonList = new ArrayList<>();
+        String pokemonFolder = "";
+        String[] pokemonFiles = listFilesForFolder(new File(pokemonFolder));
+
+
+        for (String file : pokemonFiles) {
+            String pokemonJson = readFileAsString(file);
+            JSONObject pokemon = new JSONObject(pokemonJson);
+        }
+
+
+
+
+
+
+        JSONObject =
 
 
         setPokemonEvolutions(pokemonList);
@@ -27,7 +60,7 @@ public class JsonReader {
         for (int i = 0; i < pokemonList.size(); i++) {
             Pokemon currentPokemon = pokemonList.get(i);
 
-            //returns null if there is no Pre-Evolution and sets null as well
+            //returns empty array if there is no Pre-Evolution/Post-Evolution
             List<Pokemon> preEvolution = pokemonList.stream().
                     filter(pokemon -> currentPokemon.getPreEvolutionNames().contains(pokemon.getName())).collect(Collectors.toList());
             currentPokemon.setPreEvolution(preEvolution);
